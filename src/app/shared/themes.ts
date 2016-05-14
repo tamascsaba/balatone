038d37b05d4f20d3b3c9404c62f768b206b586ee
zana <laszlo.zana@balabit.com>
2016-05-14 11:50:38 +0200

    feat(icons): add star wars icon set

--- src/app/shared/themes.ts
+++ src/app/shared/themes.ts
@@ -22,14 +22,14 @@ export const starWars = {
     millenniumFalcon
   ],
   items: [
-    {icon: 'sw1', music: ''},
-    {icon: 'sw2', music: ''},
-    {icon: 'sw3', music: ''},
-    {icon: 'sw4', music: ''},
-    {icon: 'sw5', music: ''},
-    {icon: 'sw6', music: ''},
-    {icon: 'sw7', music: ''},
-    {icon: 'sw8', music: ''}
+    {icon: 'darth-vader', music: ''},
+    {icon: 'stormtrooper', music: ''},
+    {icon: 'c-3po', music: ''},
+    {icon: 'bb-8', music: ''},
+    {icon: 'battle-droid', music: ''},
+    {icon: 'millennium-falcon', music: ''},
+    {icon: 'death-star', music: ''},
+    {icon: 'r2-d2', music: ''}
   ]
 };
 
