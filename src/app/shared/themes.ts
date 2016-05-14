f49ccac194896a0188ed71ca52a0ad875eddb8c5
zana <laszlo.zana@balabit.com>
2016-05-14 10:31:06 +0200

    feat(themes): add themes sceleton (wip)

---
+++ src/app/shared/themes.ts
@@ -0,0 +1,50 @@
+import {millenniumFalcon, formless} from "./shapes";
+
+export const blackbird = {
+  shapes: [
+    formless
+  ],
+  items: [
+    {icon: 'sw1', music: ''},
+    {icon: 'sw2', music: ''},
+    {icon: 'sw3', music: ''},
+    {icon: 'sw4', music: ''},
+    {icon: 'sw5', music: ''},
+    {icon: 'sw6', music: ''},
+    {icon: 'sw7', music: ''},
+    {icon: 'sw8', music: ''}
+  ]
+};
+
+export const starWars = {
+  shapes: [
+    formless,
+    millenniumFalcon
+  ],
+  items: [
+    {icon: 'sw1', music: ''},
+    {icon: 'sw2', music: ''},
+    {icon: 'sw3', music: ''},
+    {icon: 'sw4', music: ''},
+    {icon: 'sw5', music: ''},
+    {icon: 'sw6', music: ''},
+    {icon: 'sw7', music: ''},
+    {icon: 'sw8', music: ''}
+  ]
+};
+
+export const oneDay = {
+  shapes: [
+    formless
+  ],
+  items: [
+    {icon: 'sw1', music: ''},
+    {icon: 'sw2', music: ''},
+    {icon: 'sw3', music: ''},
+    {icon: 'sw4', music: ''},
+    {icon: 'sw5', music: ''},
+    {icon: 'sw6', music: ''},
+    {icon: 'sw7', music: ''},
+    {icon: 'sw8', music: ''}
+  ]
+};
