d60ef313f8b6eb3f61ae4edac1b15e6f24b32535
zana <laszlo.zana@balabit.com>
2016-05-15 22:37:35 +0200

    feat(themes): add 'one day' themes

--- src/app/shared/themes.ts
+++ src/app/shared/themes.ts
@@ -1,6 +1,5 @@
 import * as shapes from './shapes';
 
-
 export interface Theme {
   shapes: any;
   items: Array<Item>;
@@ -61,24 +60,24 @@ export const starWars: Theme = {
 
 export const oneDay: Theme = {
   shapes: [
+    {url: shapes.hat, icon: 'hat'},
     {url: shapes.cloud, icon: 'cloud'},
     {url: shapes.cross, icon: 'cross'},
     {url: shapes.flower, icon: 'flower'},
     {url: shapes.formless, icon: 'formless'},
-    {url: shapes.hat, icon: 'hat'},
     {url: shapes.heart, icon: 'heart'},
     {url: shapes.mustache, icon: 'mustache'},
     {url: shapes.stormCloud, icon: 'storm-cloud'}
   ],
   items: [
-    {icon: 'darth-vader', music: 'star-wars/darth-vader.mp3', x: 264.5, y: 15.5, active: true},
-    {icon: 'stormtrooper', music: 'star-wars/imperial.mp3', x: 442.5, y: 88.5, active: true},
-    {icon: 'c-3po', music: 'star-wars/lightsaber.mp3', x: 514.5, y: 265.5, active: true},
-    {icon: 'bb-8', music: 'star-wars/bb-8.mp3', x: 441.5, y: 442.5, active: true},
-    {icon: 'battle-droid', music: 'star-wars/laser.mp3', x: 264.5, y: 515.5, active: true},
-    {icon: 'millennium-falcon', music: 'star-wars/chewbacca.mp3', x: 88.5, y: 442.5, active: true},
-    {icon: 'death-star', music: 'star-wars/march.mp3', x: 14.5, y: 265.5, active: true},
-    {icon: 'r2-d2', music: 'star-wars/r2-d2.mp3', x: 87.5, y: 88.5, active: true}
+    {icon: 'moon', music: 'one-day/crickets.mp3', x: 264.5, y: 15.5, active: true},
+    {icon: 'cock', music: 'one-day/cock.mp3', x: 442.5, y: 88.5, active: true},
+    {icon: 'coffee', music: 'one-day/coffee.mp3', x: 514.5, y: 265.5, active: true},
+    {icon: 'traffic', music: 'one-day/traffic.mp3', x: 441.5, y: 442.5, active: true},
+    {icon: 'dining', music: 'one-day/dining.mp3', x: 264.5, y: 515.5, active: true},
+    {icon: 'office', music: 'one-day/office.mp3', x: 88.5, y: 442.5, active: true},
+    {icon: 'playground', music: 'one-day/playground.mp3', x: 14.5, y: 265.5, active: true},
+    {icon: 'snore', music: 'one-day/snore.mp3', x: 87.5, y: 88.5, active: true}
   ]
 };
 
