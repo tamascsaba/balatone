9ff6bda56bf341368f648fa3c0d4e0db87fe00cc
Tamas Csaba <tamcsaba@gmail.com>
2016-05-15 18:51:19 +0000

    feat(vendor): remove unused vendor and move to main

--- src/platform/browser/providers.ts
+++ src/platform/browser/providers.ts
@@ -1,10 +1,8 @@
 import {FORM_PROVIDERS} from '@angular/common';
-import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@ngrx/router';
 
 export const APPLICATION_PROVIDERS = [
   ...FORM_PROVIDERS,
-  ...HTTP_PROVIDERS,
   ...ROUTER_PROVIDERS
 ];
 
