c5b86aa64c90803260594a00660779577dfd2bf0
zana <laszlo.zana@balabit.com>
2016-05-15 20:38:13 +0200

    refact(mp3): optimize blackbird theme music size

--- src/app/shared/themes.ts
+++ src/app/shared/themes.ts
@@ -49,12 +49,12 @@ export const starWars: Theme = {
   ],
   items: [
     {icon: 'darth-vader', music: 'star-wars/darth-vader.mp3', x: 264.5, y: 15.5, active: true},
-    {icon: 'stormtrooper', music: 'star-wars/imperial.mp3', x: 442.5, y: 88.5, active: true},
+    {icon: 'stormtrooper', music: 'star-wars/march.mp3', x: 442.5, y: 88.5, active: true},
     {icon: 'c-3po', music: 'star-wars/lightsaber.mp3', x: 514.5, y: 265.5, active: true},
     {icon: 'bb-8', music: 'star-wars/bb-8.mp3', x: 441.5, y: 442.5, active: true},
     {icon: 'battle-droid', music: 'star-wars/laser.mp3', x: 264.5, y: 515.5, active: true},
     {icon: 'millennium-falcon', music: 'star-wars/chewbacca.mp3', x: 88.5, y: 442.5, active: true},
-    {icon: 'death-star', music: 'star-wars/march.mp3', x: 14.5, y: 265.5, active: true},
+    {icon: 'death-star', music: 'star-wars/imperial.mp3', x: 14.5, y: 265.5, active: true},
     {icon: 'r2-d2', music: 'star-wars/r2-d2.mp3', x: 87.5, y: 88.5, active: true}
   ]
 };
